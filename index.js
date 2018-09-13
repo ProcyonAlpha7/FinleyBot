aebd7dbbd429a848dc5fe1c6d5766a8659ba0cad
Reilly Nash <43211879+ProcyonAlpha7@users.noreply.github.com>
2018-09-13 16:16:03 -0500

    Ping/Pong
    Adds a simple call and response function

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const bot = new Discord.Client({disableEveryone: true});
 bot.on("ready",async () => {
   console.log(`${bot.user.username} is online!`);
 });
-
+  
+bot.on("message",async message => {
+  if(message.content = "ping") {
+    message.reply("pong");
+  }
+});
+  
 bot.login(botconfig.token);
 }
